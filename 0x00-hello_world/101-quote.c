10260ef7c7f953aa7ffe7c0bb9e63b9cfa5670a9
robel tade <robeltadesse37@gmail.com>
2023-03-17 11:24:10 +1200

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ * main - A c program that prints to an error
+ * Return :1 (success)
+ */
+int main(void)
+{
+ fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+ return(1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
- * main - A program that prints the size of the data types
+ * main - A program that prints the size of varius data types
+ *
  * Return: 0 (success)
  */
 int main(void)
@@ -11,10 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("size of a char: %lu byte(s)\n" (unsigned long)sizeof(a));
-printf("size of a int: %lu byte(s)\n" (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n" (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n" (unsigned long)sizeof(f));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
