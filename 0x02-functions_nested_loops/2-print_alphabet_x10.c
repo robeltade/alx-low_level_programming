ed888f58f995e80b93c7fb1d7216f6f3f8a8475e
robeltade <robeltadesse37@gmail.com>
2023-03-21 12:11:49 -0700

    functon

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 {
 char c;
 int i;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-_putchar('\n');
 for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
+_putchar('\n');
 }
 }
 
