771b194c00bfcc3beb5ac9b44af0ac916b388314
robeltade <robeltadesse37@gmail.com>
2023-03-21 16:49:15 -0700

    functions

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: aleays success
+ */
+
+int main(void)
+{
+int i = 0;
+long j = 1, k = 2;
+while (i < 50)
+{
+if (i == 0)
+printf("%d", j);
+else if (i == 1)
+printf(", %ld", k);
+else
+{
+k += j;
+j = k - j;
+printf(",%ld", k);
+}
+++i;
+}
+printf("\n");
+return (0);
+}
+
