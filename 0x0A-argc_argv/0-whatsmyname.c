ffa15a443566b27a3d783d174efe3837ee686474
robeltade <robeltadesse37@gmail.com>
2023-06-09 11:39:45 -0700

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  * @argc: value
  * @**argv: value 2
  * Return: always 0
  */
-int main(int agrc, char **argv)
+int main(int agrc, char *argv[])
 {
-
+(void) argc;
 printf("%s\n", argv[0]);
 return (0);
 }
