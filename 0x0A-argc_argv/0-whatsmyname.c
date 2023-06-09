878159c5c48320dafa761565543d825d843a4d52
robeltade <robeltadesse37@gmail.com>
2023-06-09 11:18:01 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: value
+ * @**argv: value 2
+ * Return: always 0
+ */
+int main(int agrc, char **argv)
+{
+
+printf("%s\n", argv[0]);
+return (0);
+}
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: value
+ * @argv: value
+ * Return: 0
+ */
+int main(int agrc, char **argv)
+{
+(void)agrv;
+printf("%d\n", argc - 1);
+return (0);
+}
+
