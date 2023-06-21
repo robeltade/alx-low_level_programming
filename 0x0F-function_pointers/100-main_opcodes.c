30af1448ba00abc820adf11e883507ae0afec160
robeltade <robeltadesse37@gmail.com>
2023-06-21 15:43:21 +1200

    function and pointer

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -27,7 +27,7 @@ for (index = 0; index < bytes; index++)
 opcode = *(unsigned char *)address;
 printf("%.2x", opcode);
 if (index == bytes - 1)
-{continue;}
+{continue; }
 printf(" ");
 address++;
 }
